e064b615852692a187d5081d48ba8c7734c1902b
AayushXmusic <142298501+AayushXmusic@users.noreply.github.com>
2023-09-11 13:45:00 +0530

    Update config.py
    Signed-off-by: AayushXmusic <142298501+AayushXmusic@users.noreply.github.com>

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/832943cd19e1cbfef1091.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/832943cd19e1cbfef1091.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Vw_chatting_fyting_club")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Venom_world_support")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
